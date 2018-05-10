13a1da4bb194f5a986d5269e426b21dc1ca019b7
vagrant <vagrant@homestead>
2018-05-10 03:11:16 +0000

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">由<a href="#">mingbuming</a>设计和编码</p>
+        <p class="pull-right"><a href="mailto:">联系我们</a></p>
+    </div>
+
+</footer>
